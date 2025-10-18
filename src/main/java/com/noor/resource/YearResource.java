c7158b698101d67e7e177967e886e5e6fe08e507
farnaz <farnazmmoghimi5@gmail.com>
2025-10-18 14:34:57 +0330

    Year add CRUD

--- src/main/java/com/noor/resource/YearResource.java
+++ src/main/java/com/noor/resource/YearResource.java
@@ -1,14 +1,13 @@
 package com.noor.resource;
 
-import com.noor.dao.YearRepository;
 import com.noor.entity.Year;
+import com.noor.service.YearService;
 import jakarta.inject.Inject;
 import jakarta.transaction.Transactional;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 
-import java.util.List;
 
 @Path("/years")
 @Produces(MediaType.APPLICATION_JSON)
@@ -16,25 +15,24 @@ import java.util.List;
 public class YearResource {
 
     @Inject
-    YearRepository yearRepository;
+    YearService yearService;
 
     @GET
     public Response getAll() {
-        return Response.ok(yearRepository.listAll()).build();
-    }
-
-    @GET
-    @Path("/{id}")
-    public Response get(@PathParam("id") Long id) {
-        return yearRepository.findByIdOptional(id)
-                .map(year -> Response.ok(year).build())
-                .orElse(Response.status(Response.Status.NOT_FOUND).build());
+        return Response.ok(yearService.listAll()).build();
     }
 
     @POST
     @Transactional
     public Response create(Year year) {
-        yearRepository.persist(year);
+        year=yearService.create(year);
+        return Response.ok(year).build();
+    }
+
+    @PUT
+    @Path("/{id}")
+    public Response edit(@PathParam("id") Long id, Year year) {
+        yearService.update(year);
         return Response.ok(year).build();
     }
 
@@ -42,8 +40,8 @@ public class YearResource {
     @Path("/{id}")
     @Transactional
     public Response delete(@PathParam("id") Long id) {
-        boolean deleted = yearRepository.deleteById(id);
-        return deleted ? Response.noContent().build() 
-                      : Response.status(Response.Status.NOT_FOUND).build();
+        boolean deleted = yearService.deleteById(id);
+        return deleted ? Response.noContent().build()
+                : Response.status(Response.Status.NOT_FOUND).build();
     }
 }
